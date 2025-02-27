9d087307ec65c150b6ce8162b4dbbde6260905ad
steven shyaka <s.shyaka1@alustudent.com>
2025-02-27 13:52:13 +0200

    user location

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python3
+"""
+GitHub User Location Script
 
-"""Script that prints the location of a specific user"""
+This script fetches and prints the location of a specified GitHub user
+using the GitHub API.
+
+Usage:
+    ./2-user_location.py <GitHub API URL>
+
+Requirements:
+    - Python 3.5
+    - requests module
+
+Author: Your Name
+"""
 
 import requests
 import sys
 import time
 
+def get_user_location(api_url):
+    """Fetch and print the location of a GitHub user."""
+    response = requests.get(api_url)
+    
+    if response.status_code == 404:
+        print("Not found")
+    elif response.status_code == 403:
+        reset_time = int(response.headers.get("X-RateLimit-Reset", time.time()))
+        wait_time = int((reset_time - time.time()) / 60)
+        print(f"Reset in {wait_time} min")
+    else:
+        user_data = response.json()
+        print(user_data.get("location", "Not found"))
 
 if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
-
-    if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
-        current_time = int(time.time())
-        diff = int((rate_limit - current_time) / 60)
-        print('Reset in {} min'.format(int(diff)))
-
-    elif res.status_code == 404:
-        print("Not found")
-    elif res.status_code == 200:
-        res = res.json()
-        print(res['location'])
+    if len(sys.argv) != 2:
+        print("Usage: ./2-user_location.py <GitHub API URL>")
+    else:
+        get_user_location(sys.argv[1])

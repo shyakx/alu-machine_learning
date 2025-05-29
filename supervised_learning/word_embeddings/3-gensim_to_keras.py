805c2cf89360e00b10471c0b140ee0b71503b5bd
Steven SHYAKA <133809942+shyakx@users.noreply.github.com>
2025-05-29 09:07:26 +0200

    Add files via upload

---
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,9 @@
+cript that defines a function
+    bag_of_words
+'''
+
+def gensim_to_keras(model):
+    '''
+        Converts a gensim word2vec model to a Keras Embedding layer
+    '''
+    return model.wv.get_keras_embedding(train_embeddings=True)
